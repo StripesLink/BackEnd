3d2a2830e738dd60291d5c5b7bb5ce44af248ba6
Johan Guerrero <johan.guerrero@mail.escuelaing.edu.co>
2021-05-18 23:34:07 -0500

    redis cloud update

--- src/main/java/edu/eci/escuelaing/StripesLink/Configurations/RedisConfig.java
+++ src/main/java/edu/eci/escuelaing/StripesLink/Configurations/RedisConfig.java
@@ -2,6 +2,8 @@ package edu.eci.escuelaing.StripesLink.Configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -10,7 +12,9 @@ public class RedisConfig {
 
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-		return new JedisConnectionFactory();
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("redis-stripeslink-eci-edu-co.redis.cache.windows.net", 6379);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of("koXyq+mHyH54DsDGlLS1PUNYwEIUG2a5Xo4ipzZsqyk="));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
 	}
 
 	@Bean
